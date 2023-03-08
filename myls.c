0f634c520119d93e74707395437e91b872634158
SrS3R6IO <95187440+SrS3R6IO@users.noreply.github.com>
2023-03-08 15:06:47 +0100

    Update myls.c

--- myls.c
+++ myls.c
@@ -6,12 +6,12 @@
 //#include <linux/limits.h> // Sometimes needed to be able to use PATH_MAX
 
 int main(int argc, char *argv[]){
-  char **dir = (char**)malloc(sizeof(char*)*PATH_MAX);
+  char *dir = (char*)malloc(sizeof(char*)*PATH_MAX);
   if (argc < 2)
-      *dir = getcwd(*dir,PATH_MAX); //Not assured that the path is smaller than the path_max size
+      dir = getcwd(dir,PATH_MAX); //Not assured that the path is smaller than the path_max size
   else
-      *dir = argv[1];
-  DIR * fd_open = opendir(*dir);
+      dir = argv[1];
+  DIR * fd_open = opendir(dir);
   if (fd_open == NULL) return -1; //In case the given dir is not valid
   struct dirent *current;
   while ((current = readdir(fd_open)) != NULL){
