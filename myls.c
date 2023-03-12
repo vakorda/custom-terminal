66c95509e1508e408c9022bf2b07dc2de3e3e330
vakorda <113139381+vakorda@users.noreply.github.com>
2023-03-12 13:24:33 +0100

    Update myls.c

--- myls.c
+++ myls.c
@@ -3,19 +3,20 @@
 #include <sys/types.h>	// Header file for system calls opendir, readdir y closedir
 #include <dirent.h>
 #include <stdlib.h>
-//#include <linux/limits.h> // Sometimes needed to be able to use PATH_MAX
 
 int main(int argc, char *argv[]){
+  // Allocate memory for dir
   char *dir = (char*)malloc(sizeof(char*)*PATH_MAX);
   if (argc < 2)
-      dir = getcwd(dir,PATH_MAX); //Not assured that the path is smaller than the path_max size
+      dir = getcwd(dir,PATH_MAX); // Not assured that the path is smaller than the path_max size
   else
       dir = argv[1];
   DIR * fd_open = opendir(dir);
-  if (fd_open == NULL) return -1; //In case the given dir is not valid
+  if (fd_open == NULL) return -1; // In case the given dir is not valid
   struct dirent *current;
+  // Print each file in the directory
   while ((current = readdir(fd_open)) != NULL){
-    printf("%s \n", current -> d_name);
+    printf("%s\n", current -> d_name);
   }
   return closedir(fd_open);
 }
