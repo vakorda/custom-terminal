8b1e9456347b38c98e1d34241feca314a95d081a
vakorda <113139381+vakorda@users.noreply.github.com>
2023-04-30 18:23:36 +0200

    Update queue.c

--- os_p3_multithread_2023/queue.c
+++ os_p3_multithread_2023/queue.c
@@ -9,40 +9,62 @@
 
 
 //To create a queue
-queue* queue_init(int size){
-
-	queue * q = (queue *)malloc(sizeof(queue));
-
-	return q;
+queue* queue_init(int size) {
+    queue* q = malloc(sizeof(queue));
+    q -> buffer = malloc(sizeof(element)*size);
+    q -> head = 0;
+    q -> tail = 0;
+    q -> filled = 0;
+    for(int i=0; i < size; i++) {
+        element e;
+        q -> buffer[i] = e;
+    }
+    q -> size = size;
+    return q;
 }
 
 
 // To Enqueue an element
 int queue_put(queue *q, struct element* x) {
-	return 0;
+    if(q -> filled != q -> size) {
+        if (q -> filled == 0) {
+            q -> buffer[q -> tail] = *x;
+        } else {
+            q -> buffer[q -> tail + 1] = *x;
+        }
+        q -> tail = (q -> tail + 1) % q -> size;
+        q -> filled = (q -> tail - q -> head) % q -> size;
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
 
 // To Dequeue an element.
 struct element* queue_get(queue *q) {
-	struct element* element;
-	
-	return element;
+    if (q -> filled != 0) {
+        element *head = &q -> buffer[q -> head];
+        q -> head = (q -> head + 1) % q -> size;
+        q -> filled = (q -> tail - q -> head) % q -> size;
+        return head;
+    }
+    return (element *) (- 1);
 }
 
 
 //To check queue state
 int queue_empty(queue *q){
-	
-	return 0;
+    return !(q -> filled == 0);
 }
 
 int queue_full(queue *q){
-	
-	return 0;
+    return !(q -> filled == q -> size);
 }
 
 //To destroy the queue and free the resources
 int queue_destroy(queue *q){
-	return 0;
+    free(q -> buffer);
+    free(q);
+    return 0;
 }
