13725053091bf37aef7c4119210961fd2bac43a1
SrS3R6IO <95187440+SrS3R6IO@users.noreply.github.com>
2023-05-04 16:23:17 +0000

    Update queue.c

--- os_p3_multithread_2023/queue.c
+++ os_p3_multithread_2023/queue.c
@@ -33,12 +33,13 @@ int queue_put(queue *q, struct element* x) {
         if (q -> filled == 0) {
             q -> buffer[q -> tail] = *x;
         } else {
-            q -> buffer[(q -> tail + 1) % (q -> size)] = *x;
             q -> tail = (q -> tail + 1) % q -> size;
+            q -> buffer[q -> tail] = *x;
+
         }
         q -> filled = q -> tail - q -> head + 1;
-        if (q -> filled < 0) {
-            q -> filled = q -> size + q -> filled + 1;
+        if (q -> filled <= 0) {
+            q -> filled = q -> size + q -> filled;
         }
         //printf("af: num_elems = %d, tail= %d, head = %d, size = %d\n", q -> filled, q -> tail, q -> head, q -> size);
 
@@ -62,8 +63,8 @@ struct element* queue_get(queue *q) {
 
             q -> filled = q -> tail - q -> head + 1;
 
-            if (q -> filled < 0) {
-                q -> filled = q -> size + q -> filled + 1;
+            if (q -> filled <= 0) {
+                q -> filled = q -> size + q -> filled;
             }
         }
 
@@ -86,12 +87,76 @@ int queue_full(queue *q){
 //To destroy the queue and free the resources
 int queue_destroy(queue *q){
     free(q -> buffer);
+    //free(q);
     return 0;
 }
 
 int print_elems(queue* q) {
     for(int i=0; i<q -> size; i++) {
-        printf("%d:[%s]  ", i, q -> buffer[i].operation);
+        if(q -> head == q -> tail){
+            if(q -> filled == 1 && i == q -> head) {
+                printf("%d:[%s]  ", i, q -> buffer[i].operation);
+            } else {
+                printf("\033[1;31m");
+                printf("%d:[%s]  ", i, q -> buffer[i].operation);
+                printf("\033[0;29m");
+            }
+        }
+        else if(q -> head < q -> tail) {
+            if(!(q -> head <= i && i <= q -> tail)){
+                printf("\033[1;31m");
+                printf("%d:[%s]  ", i, q -> buffer[i].operation);
+                printf("\033[0;29m");
+            } else printf("%d:[%s]  ", i, q -> buffer[i].operation);
+        }
+        else {
+            if((q -> tail < i && i < q -> head)){
+                printf("\033[1;31m");
+                printf("%d:[%s]  ", i, q -> buffer[i].operation);
+                printf("\033[0;29m");
+            } else printf("%d:[%s]  ", i, q -> buffer[i].operation);
+        }
     }
     printf("\n");
 }
+
+/*
+int print_cycle(queue* q) {
+    int pos = q -> head;
+    printf("\033[0;30m");
+    printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+    printf("\033[0;29m");
+    pos = (pos + 1) % q -> size;
+    while(pos != q -> tail) {
+        printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+        pos = (pos + 1) % q -> size;
+    }
+    pos = q -> head;
+    printf("\n\033[1;32m");
+    printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+    printf("\033[0;30m");
+    if(q -> filled >= (q -> size - q -> filled + 2)){
+        while(pos != q -> tail) {
+            printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+            pos = (pos + 1) % q -> size;
+        }
+    } else {
+        pos = q -> tail;
+        pos = (pos + 1) % q -> size;
+        while(pos != q -> head) {
+            printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+            pos = (pos + 1) % q -> size;
+        }
+        pos = q -> tail;
+    }
+    printf("\033[1;33m");
+    printf("%d:[%s]\n\033[0;30m", pos, q -> buffer[pos].operation);
+    printf("%d:[%s]  \033[1;31m", pos, q -> buffer[q -> head].operation);
+    pos = (pos + 1) % q -> size;
+    while(pos != q -> head) {
+        printf("%d:[%s]  ", pos, q -> buffer[pos].operation);
+        pos = (pos + 1) % q -> size;
+    }
+    printf("\n");
+}
+*/
