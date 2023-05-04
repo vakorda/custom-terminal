cf98b9a4cd74f861a7df58eea91c8eeba6d7f112
SrS3R6IO <95187440+SrS3R6IO@users.noreply.github.com>
2023-05-04 08:23:13 +0000

    Update bank.c

--- os_p3_multithread_2023/bank.c
+++ os_p3_multithread_2023/bank.c
@@ -30,12 +30,10 @@ char ** list_clients_ops;
 long int ** account_balance;
 int n_commands;
 int max_accounts;
-//sem_t mu;
-//sem_t sem_producer;
-//sem_t sem_consumer;
+
 pthread_mutex_t mutex;
-pthread_cond_t full;
-pthread_cond_t empty;
+pthread_cond_t no_full;
+pthread_cond_t no_empty;
 
 
 
@@ -99,24 +97,24 @@ int check_arguments(int argc, const char *argv[]) {
 }
 
 int check_argument(char * line){
-	if(!atoi(line) && strncmp(line, "0", 2)) {
-		perror("Number of ATMs must be a number!!\n");
-		exit(-1);
-	    }
+        if(!atoi(line) && strncmp(line, "0", 2)) {
+                perror("Number of ATMs must be a number!!\n");
+                exit(-1);
+            }
 }
 
 
 void create_account(int num_account) {
-    if(account_balance[num_account] != NULL) {
+    if(account_balance[num_account - 1] != NULL) {
         perror("Account already exists!");
         exit(-1);
     }
-    account_balance[num_account] = malloc(sizeof(long int));
-    *account_balance[num_account] = 0;
+    account_balance[num_account - 1] = malloc(sizeof(long int));
+    *account_balance[num_account - 1] = 0;
 }
 
 void error_if_account_exists(int num_account) {
-    if(account_balance[num_account] == NULL) {
+    if(account_balance[num_account - 1] == NULL) {
         perror("Account does not exist!");
         exit(-1);
     }
@@ -124,40 +122,40 @@ void error_if_account_exists(int num_account) {
 
 void deposit(int num_account, int ammount) {
     error_if_account_exists(num_account);
-    *account_balance[num_account] += ammount;
+    *account_balance[num_account - 1] += ammount;
     global_balance += ammount;
 }
 
 void withdraw_money(int num_account, int ammount) {
     error_if_account_exists(num_account);
-    *account_balance[num_account] -= ammount;
+    *account_balance[num_account - 1] -= ammount;
     global_balance -= ammount;
 }
 
 void transfer(int num_account1, int num_account2, int ammount) {
     error_if_account_exists(num_account1);
     error_if_account_exists(num_account2);
-    *account_balance[num_account1] -= ammount;
-    *account_balance[num_account2] += ammount;
+    *account_balance[num_account1 - 1] -= ammount;
+    *account_balance[num_account2 - 1] += ammount;
 
 }
 
 void print_account(int num_account) {
-    printf("GLOBAL BALANCE: %lld\n-----------\nACCOUNT %d\nMONEY = %ld\n-----------\n", global_balance, num_account, *account_balance[num_account]);
+    printf("GLOBAL BALANCE: %lld\n-----------\nACCOUNT %d\nMONEY = %ld\n-----------\n", global_balance, num_account, *account_balance[num_account - 1]);
 }
 
 void teachers_print(int num_accounts,char * instruction){
-    printf("%d %s BALANCE = %ld TOTAL = %lld", bank_numop, instruction, *account_balance[num_accounts], global_balance);
+    printf("%d %s BALANCE = %ld TOTAL = %lld", bank_numop, instruction, *account_balance[num_accounts - 1], global_balance);
 }
 
 void print_all_accounts(int mark1, int mark2) {
     printf(" ACCOUNT  |  MONEY\n"
            "--------------------\n");
-    for(int i=1; i < max_accounts; i++)
-        if(account_balance[i] != NULL) {
+    for(int i=1; i <= max_accounts; i++)
+        if(account_balance[i - 1] != NULL) {
             if(i == mark1) printf("\033[1;31m");
             else if(i == mark2) printf("\033[1;32m");
-            printf(" %-11d %-5ld$\n", i, *account_balance[i]);
+            printf(" %-11d %-5ld$\n", i, *account_balance[i - 1]);
             if(i == mark1 || i == mark2) printf("\033[0;29m");
         }
     printf("--------------------\n"
@@ -166,44 +164,44 @@ void print_all_accounts(int mark1, int mark2) {
 
 void do_action(char* operation) {
     char ** line = (char **)malloc(sizeof(char*)*4);
-    
+
     printf("\033[1;33m%s\n\033[0;29m", operation);
     char * op = strtok(operation," ");
     int i = 0;
     while (op != NULL && i < 4){
-    	line[i] = op;
-    	op = strtok(NULL," ");
-    	i++;
+        line[i] = op;
+        op = strtok(NULL," ");
+        i++;
     }
-    
+
     if (strncmp(line[0], "CREATE", 7) == 0) {
-    	check_argument(line[1]);
-    	
+        check_argument(line[1]);
+
         create_account(atoi(line[1]));
         print_account(atoi(line[1]));
     } else if (strncmp(line[0], "DEPOSIT", 8) == 0) {
-	for (int i = 1; i < 3;i++){
-	    check_argument(line[i]);
-	   }
+        for (int i = 1; i < 3;i++){
+            check_argument(line[i]);
+           }
         deposit(atoi(line[1]), atoi(line[2]));
         print_account(atoi(line[1]));
 
     } else if (strncmp(line[0], "WITHDRAW", 9) == 0){
-	for (int i = 1; i < 3;i++){
-	    check_argument(line[i]);
-	   }
+        for (int i = 1; i < 3;i++){
+            check_argument(line[i]);
+           }
         withdraw_money(atoi(line[1]), atoi(line[2]));
         print_account(atoi(line[1]));
     }
     else if (strncmp(line[0], "TRANSFER", 9) == 0){
-    	for (int i = 1; i < 4;i++){
-	    check_argument(line[i]);
-	   }
+        for (int i = 1; i < 4;i++){
+            check_argument(line[i]);
+           }
         transfer(atoi(line[1]), atoi(line[2]), atoi(line[3]));
-        print_all_accounts(atoi(line[2]), atoi(line[2]));
+        print_all_accounts(atoi(line[1]), atoi(line[2]));
     }
     else if (strncmp(line[0], "BALANCE", 8) == 0){
-    	check_argument(line[1]);
+        check_argument(line[1]);
         print_account(atoi(line[1]));
     }
     else {
@@ -218,49 +216,43 @@ void producer(queue *q) {
         //sem_wait(&mu);
         pthread_mutex_lock(&mutex);
         while (queue_full(q)==0){
-        	pthread_cond_wait(&full, &mutex);
+            pthread_cond_wait(&no_full, &mutex);
         }
-        element o;
-        o.operation = list_clients_ops[client_numop++];
-        queue_put(q, &o);
-        
-        pthread_cond_signal(&empty);
+        if(client_numop < n_commands){
+            element o;
+            o.operation = list_clients_ops[client_numop];
+            queue_put(q, &o);
+            client_numop++;
+            printf("client_numop: %d\n",client_numop);
+            print_elems(q);
+            pthread_cond_signal(&no_empty);
+        }
         pthread_mutex_unlock(&mutex);
-        
-        // print_elems(q);
-
-        //sem_post(&mu);
-        //sem_post(&sem_consumer);
     }
+    printf("\033[1;36mPRODUCER FINNISHED\033[0;29m\n");
     pthread_exit(0);
 }
 
 void consumer(queue *q) {
      while(bank_numop < n_commands){
-        //sem_wait(&sem_consumer);
-
-        //sem_wait(&mu);
-
-
         pthread_mutex_lock(&mutex);
-        
-        while (queue_empty(q)==0){
-        	pthread_cond_wait(&empty, &mutex);
-        }
-        element o;
-        o = *queue_get(q);
-        do_action(o.operation);
-        printf("DID OPERATION %s\n", o.operation);
-        bank_numop++;
-        print_elems(q);
-        pthread_cond_signal(&full);
-        pthread_mutex_unlock(&mutex);
-        
-       
-        //sem_post(&mu);
 
-        //sem_post(&sem_producer);
+        while (queue_empty(q)==0 && bank_numop < n_commands){
+            pthread_cond_wait(&no_empty, &mutex);
+        }
+        if(bank_numop < n_commands){
+            element o;
+            o = *queue_get(q);
+            do_action(o.operation);
+            printf("DID OPERATION %s\n", o.operation);
+            print_elems(q);
+            bank_numop++;
+            printf("bank_numop: %d\n",bank_numop);
+            pthread_cond_signal(&no_full);
+        }else pthread_cond_signal(&no_empty);
+        pthread_mutex_unlock(&mutex);
     }
+    printf("\033[1;36mCONSUMER FINNISHED\033[0;29m\n");
     pthread_exit(0);
 }
 
@@ -269,7 +261,11 @@ int main (int argc, const char * argv[] ) {
     check_arguments(argc, argv);
     int prods = atoi(argv[2]);
     int cons = atoi(argv[3]);
-    //printf("prods: %d, cons: %d\n", prods, cons);
+    if (prods == 0 || cons == 0) {
+    	perror("PRODUCERS AND CONSUMERS CANT BE 0");
+    	exit(-1);
+    }
+    
     max_accounts = atoi(argv[4]);
     int buff_size = atoi(argv[5]);
 
@@ -277,15 +273,10 @@ int main (int argc, const char * argv[] ) {
 
     n_commands = init_list_clients(argv[1], list_clients_ops);
 
-    //sem_init(&mu, 0, 1);
-    //sem_init(&sem_producer, 0, 1);
-    //sem_init(&sem_consumer, 0, 0);
-    //printf("%d\n",n_commands);
-    pthread_mutex_init(&mutex, NULL);
-    pthread_cond_init(&full, NULL);
-    pthread_cond_init(&empty, NULL);
 
-    
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&no_full, NULL);
+    pthread_cond_init(&no_empty, NULL);
 
     queue q;
     q = *queue_init(buff_size);
@@ -295,7 +286,7 @@ int main (int argc, const char * argv[] ) {
     for(int i = 0;  i < prods; i++){
         pthread_create(&thid_atm[i], NULL, (void *)producer, &q);
     }
-    
+
     for(int i = 0;  i < cons; i++){
         pthread_create(&thid_workers[i], NULL, (void *)consumer, &q);
     }
@@ -303,20 +294,19 @@ int main (int argc, const char * argv[] ) {
     for(int i = 0;  i < prods; i++){
         pthread_join(thid_atm[i], NULL);
     }
-    
+
     for(int i = 0;  i < cons; i++){
         pthread_join(thid_workers[i], NULL);
     }
 
     queue_destroy(&q);
-    
-    //sem_destroy(&mu);
 
     pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&full);
-    pthread_cond_destroy(&empty);
-    
+    pthread_cond_destroy(&no_full);
+    pthread_cond_destroy(&no_empty);
+
     free(list_clients_ops);
     free(account_balance);
+    
     return 0;
 }
